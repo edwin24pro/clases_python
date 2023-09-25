c85b3bd05e46afe033300b4917bfb5724aaf4b5c
edwin <ramosaguilaredwin7@gmail.com>
2023-09-25 12:01:12 -0500

    ediciones

--- librerias_framework/clases/Repaso/ejercicioi4.py
+++ librerias_framework/clases/Repaso/ejercicioi4.py
@@ -1,9 +1,10 @@
 ##Escriba una funcion que reciba un numero entero positivo y devuelva sufactorial.
-num = 2
+num = 4
+resul = 0
 for i in range(num):
     factorial = num-i
-    factorial += factorial
-    print(factorial)
+    resul= resul * factorial
+print(resul)
 
 #7. Escribir una funcion que reciva como parametros una lista de nuemros y retorne una
 #nueva lista con el cuadro de cada numero de la lista ingresada.
@@ -18,5 +19,6 @@ print(lista_nueva)
 ## 8. Escribir un programa que reciba una cadena de caracteres y devuelva
 #  un objeto con cada palabra que contiene y su frecuencia.
 nombre = 's o l'
+for i in nombre:
 objeto = nombre.object(' ')
 print(objeto)
