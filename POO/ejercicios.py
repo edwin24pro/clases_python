4fcf81dd57b28eee9b52f8436d4d296a21e034a3
yerson34 <grifincolondormilon@email.com>
2023-10-03 11:35:50 -0500

    ejercicios

--- POO/ejercicios.py
+++ POO/ejercicios.py
@@ -1,4 +1,4 @@
-##Haciendo uso de la POO crear un objeto para la entidad celular
+##1.Haciendo uso de la POO crear un objeto para la entidad celular
 class Celular:
 
     marca = 'huawei'
@@ -13,7 +13,7 @@ dispositivo = Celular()
 print(dispositivo.boton1('Activado'))
 
 
-##Haciendo uso de la POO crear un objeto para la entidad vehiculo
+##2.Haciendo uso de la POO crear un objeto para la entidad vehiculo
 class Vehiculo:
 
     marca = 'volvage'
@@ -21,13 +21,13 @@ class Vehiculo:
     color = 'rojo'
 
     def llanta(self, estado):
-        power = f'El celular esta {estado}'
+        power = f'La llanta esta {estado}'
         return power
 
 dispositivo = Vehiculo()
-print(dispositivo.boton1('Activado'))
+print(dispositivo.llanta('Buena'))
 
-##Haciendo uso de la POO crear un objeto para la entidad avion
+##3.Haciendo uso de la POO crear un objeto para la entidad avion
 class Avion:
 
     marca = 'airubus'
@@ -35,22 +35,69 @@ class Avion:
     color = 'rojo'
 
     def llanta(self, estado):
-        power = f'El celular esta {estado}'
+        power = f'La llanta esta {estado}'
         return power
 
 dispositivo = Avion()
-print(dispositivo.boton1('Activado'))
+print(dispositivo.llanta('Ponchado'))
 
-##Haciendo uso de la POO crear un objeto para la entidad dota2
+##4.Haciendo uso de la POO crear un objeto para la entidad dota2
 class Dota2:
 
     personaje = 'Abaddon'
-    motor = 'corean'
+    trotofeo = 'corean'
     color = 'rojo'
 
-    def llanta(self, estado):
-        power = f'El celular esta {estado}'
+    def arma(self, estado):
+        power = f'La espada esta {estado}'
         return power
 
-dispositivo = Avion()
-print(dispositivo.boton1('Activado'))
+dispositivo =Dota2()
+print(dispositivo.arma('Rota'))
+#5.Haciendo uso de la POO crear un objeto para PC
+class Pc:
+    monitor = 'bueno'
+    case = 'chatarra'
+    mouse = 'en buen estado'
+    teclado = 'en buen estado'
+
+    def cpu(self, pregunta):
+        pregunta = input('Que cosa realiza un CPU?: ')
+        return pregunta
+artefacto = Pc()
+print(artefacto.cpu(1))
+##6.Haciendo uso de la POO crear un objeto para una impresora
+class Impresora:
+    bandeja = 'grande'
+    case = 'chatarra'
+    mouse = 'en buen estado'
+    teclado = 'en buen estado'
+
+    def bandeja_hojas(self, bandej):
+        estado = bandej
+        return estado
+
+artefacto2 = Impresora()
+print(artefacto2.bandeja_hojas(bandej='a'))
+
+##7.Haciendo uso de la POO crear un objeto para emitir una factura
+class Factura:
+    producto = 'soda'
+    cantidad = 'en buen estado'
+    precio = 'tres soles'
+
+    def total_pagar(self, cantidad):
+        precio = 3
+        total_pagar = cantidad * precio
+        return f'por comprar soda total_pagar {total_pagar}'
+    
+
+boletos = Factura()
+print(f'''
+      ---------------------------------------------------------
+      |{boletos.precio} {boletos.cantidad}  {boletos.producto}|
+      |                                                       |
+      |                   {boletos.total_pagar(cantidad=30)}  |
+      |                                                       |
+      ---------------------------------------------------------
+''')
