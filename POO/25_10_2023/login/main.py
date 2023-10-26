0a1a6f69639b54675b2bfd969618ff1749e5ef07
edwin <ramosaguilaredwin7@gmail.com>
2023-10-26 16:09:06 -0500

    base de datos de usuarios, funciones calcular, actualizar, comprovar usuario y login usuario

--- POO/25_10_2023/login/main.py
+++ POO/25_10_2023/login/main.py
@@ -6,27 +6,50 @@ from bd import *
 # verificar si usuario esta registrado o existe en mis registros
 # validar usuario y password
 #generamos la edad
-def calcular_edad(dni, bd_user):
-	calcular_edad = list(filter(lambda el : el['dni']==dni, bd_user))
-	fecha_nacimiento = int(calcular_edad[0]['fecha_nacimiento'][6:])
-	año_actual = 2023
-	edad_actual = año_actual - fecha_nacimiento
-	return edad_actual
-
-edad_actual = calcular_edad('4', usuarios)
 
 class Usuario:
-  '''
-  def  actualizar_edad(self, dni, bd_users, clave, valor_nuevo):
-    	actualizar_edad = list(filter(lambda el: el['dni']==dni, bd_users))[0].update({clave:valor_nuevo})
-    	return 'se actualizo'
-    	'''
-  def usuario_existe(self, dni, usuario, bd_ users):
-    	usuario_existe = list(filter(lambda el : el['dni'] == dni and el['usuario']==usuario, bd_users))
-    	return usuario_existe
-    	
-registro = Usuario()
+	def __init__(self, bd_users):
+		self.bd_users = bd_users
 
-#print(registro.actualizar_edad('4', usuarios, 'edad', edad_actual))
-print(registro.usuario_existe('4', 'jory', usuarios))
+	def calcular_edad(self, dni):
+		calcular_edad = list(filter(lambda el : el['dni']==dni, self.bd_users))
+		fecha_nacimiento = int(calcular_edad[0]['fecha_nacimiento'][6:])
+		año_actual = 2023
+		edad_actual = año_actual - fecha_nacimiento
+		return edad_actual
+
+	def actualizar_edad(self, dni, bd_users, clave, valor_nuevo):
+		actualizar_edad = list(filter(lambda el: el['dni']==dni, bd_users))[0].update({clave:valor_nuevo})
+		return 'se actualizo'
+    	
+	def usuario_existe(self, dni, usuario, bd_users):
+		usuario_existe = bool(list(filter(lambda el : el['dni'] == dni and el['usuario']==usuario, bd_users)))
+		if usuario_existe == True:
+			return f'el usuario {usuario} existe'
+		else:
+			return f'el usuario {usuario} no existe'
+	
+	def login_usuario(self, usuario, password):
+		usuario_login = list(filter(lambda el:el['usuario'] == usuario and el['password'] == password, self.bd_users))
+		decicion = bool(usuario_login)
+		if decicion == True:
+			return f'''
+			bienvenido a su perfil!
+			
+			aqui esta sus cosas: 😊
+			---------------------------------------------------------------------------------------------------------------------
+			{usuario_login}
+			---------------------------------------------------------------------------------------------------------------------
+			'''
+		else:
+			return 'Su usuario y contraseña son incorrectos'
+
+registro = Usuario(usuarios)
+#edad_actual = calcular_edad('4', usuarios)
+
+registro.actualizar_edad('4', usuarios, 'edad', registro.calcular_edad('4'))
+#print(registro.usuario_existe('4', 'jory', usuarios))
+#print(registro.calcular_edad('4'))
+#print(registro.login_usuario('jory','a111'))
+print(usuarios)
 
