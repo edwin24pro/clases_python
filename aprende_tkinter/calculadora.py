018a64a449cbf9556ee5d5b1d51d261df99f8185
edwin24pro <134083365+edwin24pro@users.noreply.github.com>
2023-11-02 21:14:04 -0500

    Delete aprende_tkinter directory

--- aprende_tkinter/calculadora.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk, Button, Label, Entry
-ventana = Tk()
-ventana.title('Calculadora new')
-ventana.geometry('300x350')
-etiqueda_saludo = Label(ventana, text= 'Bienvenido aquí')
-etiqueda_saludo.grid(column=0, row=0, padx=2, pady=2)
-
-
-ventana.mainloop()
