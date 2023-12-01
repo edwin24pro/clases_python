1497e0e0d12f906298831df25f9c31ffc7a2d4e5
edwin <ramosaguilaredwin7@gmail.com>
2023-12-01 08:13:30 -0500

    tarea de insersion de los metodos de orm en nuestro app diario

--- primera_app/funciones.py
+++ primera_app/funciones.py
@@ -25,15 +25,16 @@ def nuevo(ventana):
     db.insertarUno('Usuarios',date)
     showinfo(title='save', message='nuevo registro agregado')
     #nuevo
-    ventana.tabla_datos.insert('',END, text=name, values=(apellido,celular))
+    id = db.mostrar('Usuarios', where=f'Celular={celular}')[0][0]
+    ventana.tabla_datos.insert('',END, text=id, values=(name,apellido,celular))
     limpiar(ventana)
 #eliminar 
 def eliminar(ventana):
     elemento_eliminar = ventana.tabla_datos.selection()
     dato = ventana.tabla_datos.item(elemento_eliminar)['text'] #sirve para mostrar el registro seleccionado
     ventana.tabla_datos.delete(elemento_eliminar)
-    db.eliminar('Usuarios',where="id='dato'")
-    #db.eliminar('Usuarios',where=)
+    db.eliminar('Usuarios',where=f'id= "{dato}"')
+    #db.eliminar('Usuarios',where='Nombre="ggh"')
     showwarning(title='Delete', message='registro eliminado')
 
 def actualizar(ventana):
@@ -44,13 +45,18 @@ def actualizar(ventana):
         apellidos = ventana.apellido_texto.get()
         celular = ventana.celular_texto.get()
         elemento_actualizar = ventana.tabla_datos.selection()
-        date = ventana.tabla_datos.item(elemento_actualizar)
+        date = ventana.tabla_datos.item(elemento_actualizar)['text']
         mensaje = askyesno(title='actualizar', message='estas seguro que deseas actulizar esta hvda')
         if mensaje == True:
             limpiar(ventana)
+            update = {
+                'Nombre':nombre,
+                'Apellido':apellidos,
+                'Celular':celular
+                }
             ventana.tabla_datos.selection_remove(elemento_actualizar)
-            db.actualizar('Usuarios',date,where="id='{date['text']}'")
-            return ventana.tabla_datos.item(elemento_actualizar, text=nombre, values=(apellidos,celular))
+            db.actualizar('Usuarios',update,where=f'id={date}')
+            return ventana.tabla_datos.item(elemento_actualizar,text=date, values=(nombre,apellidos,celular))
         else:
             showinfo(title='no actualizo', message='no esta seleccionado')
             ventana.tabla_datos.selection_remove(elemento_actualizar)
@@ -61,9 +67,9 @@ def doble_clic(ventana,event):
     captura_datos = ventana.tabla_datos.item(elemento_actualizar)
     mensaje = askyesno(title='actualizar', message='desde el registro')
     if mensaje == True:
-        nombre = captura_datos['text']
-        apellidos = captura_datos['values'][0]
-        celulular = captura_datos['values'][1]
+        nombre = captura_datos['values'][0]
+        apellidos = captura_datos['values'][1]
+        celulular = captura_datos['values'][2]
         ventana.nombre_texto.insert(0,nombre)
         ventana.apellido_texto.insert(0,apellidos)
         ventana.celular_texto.insert(0,celulular)
